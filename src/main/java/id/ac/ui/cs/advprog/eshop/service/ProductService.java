206b110f29f9ab9a1b5d68dd4c6eb0c484e0c06f
unknown <zazaneryawan@gmail.com>
2024-02-14 12:02:52 +0700

    Fix Some PMD Code Analysis problems

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -3,9 +3,9 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 public interface ProductService {
-    public Product create(Product product);
-    public Product delete(int index);
-    public Product deleteByProductId(String productId);
+    Product create(Product product);
+    Product delete(int index);
+    Product deleteByProductId(String productId);
     public Product getProductByProductId(String productId);
     public Product edit(Product product);
     public List<Product> findAll();
