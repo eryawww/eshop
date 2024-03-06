f868b59e72e917962fc83c68a53e49f929b118cf
unknown <zazaneryawan@gmail.com>
2024-03-06 20:24:28 +0700

    [GREEN] Implement PaymentServiceImpl class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,14 +1,15 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -17,21 +18,31 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData) {
+        if (paymentRepository.findById(paymentId) == null){
+            Payment payment = new Payment(paymentId, method, order, paymentData);
+            paymentRepository.save(payment);
+            return payment;
+        }
         return null;
     }
-
     @Override
     public Payment setStatus(Payment payment, String status) {
+        Order currentOrder = payment.getOrder();
+        if (status.equals("SUCCESS")) {
+            currentOrder.setStatus(OrderStatus.SUCCESS.getValue());
+        } else if (status.equals("REJECTED")) {
+            currentOrder.setStatus(OrderStatus.FAILED.getValue());
+        } else {
+            throw new IllegalArgumentException("Invalid status payment");
+        }
         return payment;
     }
-
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
-
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.getAllPayment();
     }
 }
